356719d7a2519f94e16c035ee4e9b63e9ef8732a
Hesham El Masry <heshamelmasry1992@gmail.com>
2025-11-07 17:19:15 +0200

    add husky - prettier - ESLint config fix - run prettier and eslint set husky

--- src/routes/root/RootLayout.tsx
+++ src/routes/root/RootLayout.tsx
@@ -1,9 +1,13 @@
 import { Outlet, Link } from "react-router-dom";
 export default function RootLayout() {
-    return (
-        <div>
-            <header className="p-4 border-b"><Link to="/">Fruit Basket</Link></header>
-            <main className="p-4"><Outlet /></main>
-        </div>
-    );
+  return (
+    <div>
+      <header className="p-4 border-b">
+        <Link to="/">Fruit Basket</Link>
+      </header>
+      <main className="p-4">
+        <Outlet />
+      </main>
+    </div>
+  );
 }
